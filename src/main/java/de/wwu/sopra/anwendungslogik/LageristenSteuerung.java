6c354d2ef751c5f2c1dca547a25c49108ce61c7d
Noah Latzel <nlatzel@uni-muenster.de>
2022-02-24 14:03:25 +0100

    Funktionen teilweise  ausprogrammiert

--- src/main/java/de/wwu/sopra/anwendungslogik/LageristenSteuerung.java
+++ src/main/java/de/wwu/sopra/anwendungslogik/LageristenSteuerung.java
@@ -5,13 +5,26 @@ import java.util.List;
 
 import de.wwu.sopra.datenhaltung.bestellung.Bestellung;
 import de.wwu.sopra.datenhaltung.management.Fahrzeug;
+import de.wwu.sopra.datenhaltung.management.Lager;
 import de.wwu.sopra.datenhaltung.management.Route;
 
 public class LageristenSteuerung {
+	Lager lager;
 
-	// TODO In welchem Format wird die Nachbestellung uebergeben?
-	public void bestelleNach() {
-
+	// TODO Preisliste importieren fuer Grosshaendler?
+	// TODO Ausgaben mit Statistiken verrechnen
+	/**
+	 * Die Methode fuegt alle Produkte der Nachbestellungen in der gewuenschten
+	 * Menge dem Lager hinzu
+	 * 
+	 * @param nachbestellungen Die Nachbestellungen, die verarbeitet werden sollen.
+	 */
+	public void bestelleNach(HashSet<NachbestellungTupel> nachbestellungen) {
+		for (NachbestellungTupel n : nachbestellungen) {
+			for (int i = 0; i < n.getMenge(); i++) {
+				lager.addProdukt(n.getProdukt().clone());
+			}
+		}
 	}
 
 	/**
@@ -24,8 +37,9 @@ public class LageristenSteuerung {
 	 * 
 	 * TODO Invariante fuer Bestellung: niemals leer sein TODO Erfolgsnachricht?
 	 * 
-	 * @param bestellungen
-	 * @param fahrzeug
+	 * @param bestellungen Die Bestellungen, die in eine Route eingeplant werden und
+	 *                     vorher im GUI ausgewaehlt wurden.
+	 * @param fahrzeug     Das Fahrzeug, dem Bestellungen zugeordnet werden.
 	 */
 	public void planeRoute(List<Bestellung> bestellungen, Fahrzeug fahrzeug) {
 		int gesamtBelegung = 0;
@@ -42,6 +56,13 @@ public class LageristenSteuerung {
 		route.setBestellungen(bestellungen);
 	}
 
+	/**
+	 * Zeigt die Route eines ausgewaehlten Fahrzeugs. Das Fahrzeug muss eine Route
+	 * haben, sonst wird eine IllegalArgumentException geworfen.
+	 * 
+	 * @param fahrzeug Das Fahrzeug, dessen Route angezeigt werden soll.
+	 * @return Die Route des Fahrzeugs, falls sie existiert.
+	 */
 	public Route zeigeRouteVonFahrzeug(Fahrzeug fahrzeug) {
 		Route route = fahrzeug.getRoute();
 		if (route == null) {
