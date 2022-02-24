31eb9f273b4724e8c6ae4a2dcf75b40c800c1a00
Noah Latzel <nlatzel@uni-muenster.de>
2022-02-24 14:02:04 +0100

    Tupel fuer Nachbestellung hinzugefuegt

---
+++ src/main/java/de/wwu/sopra/anwendungslogik/NachbestellungTupel.java
@@ -0,0 +1,39 @@
+package de.wwu.sopra.anwendungslogik;
+
+import de.wwu.sopra.datenhaltung.management.Produkt;
+
+/**
+ * 
+ * @author NoahLatzel
+ *
+ *         Die Klasse dient zur Realisierung der Nachbestellung. Das Tupel
+ *         enthaelt ein Produkt und eine Menge. Diese Tupel wird dann der
+ *         Funktion bestelleNach() in der LageristenSteuerung uebergbene.
+ */
+public class NachbestellungTupel {
+	private final Produkt produkt;
+	private final int menge;
+
+	public NachbestellungTupel(Produkt produkt, int menge) {
+		this.produkt = produkt;
+		this.menge = menge;
+	}
+
+	/**
+	 * Gibt das Produkt der Nachbestellung zurueck.
+	 * 
+	 * @return Die Bestellung
+	 */
+	public Produkt getProdukt() {
+		return this.produkt;
+	}
+
+	/**
+	 * Gibt die Menge der Nachbestellung zurueck.
+	 * 
+	 * @return Die Menge
+	 */
+	public int getMenge() {
+		return this.menge;
+	}
+}
