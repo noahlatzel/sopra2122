5610619fc099f68de76770213b323efc1bbc60b5
Noah Latzel <nlatzel@uni-muenster.de>
2022-02-25 10:08:55 +0100

    Fahrzeugregister implementiert und getestet

--- src/main/java/de/wwu/sopra/datenhaltung/verwaltung/FahrzeugRegister.java
+++ src/main/java/de/wwu/sopra/datenhaltung/verwaltung/FahrzeugRegister.java
@@ -1,5 +1,9 @@
 package de.wwu.sopra.datenhaltung.verwaltung;
 
+import java.util.HashSet;
+
+import de.wwu.sopra.datenhaltung.management.Fahrzeug;
+
 /**
  * Die Klasse verwaltet die Speicherung aller Fahrzeugdaten, also unter anderem
  * Routen und Fahrzeuge.
@@ -8,5 +12,32 @@ package de.wwu.sopra.datenhaltung.verwaltung;
  *
  */
 public class FahrzeugRegister {
+	private HashSet<Fahrzeug> fahrzeuge = new HashSet<Fahrzeug>();
 
+	/**
+	 * Fuegt ein Fahrzeug dem Register hinzu.
+	 * 
+	 * @param fahrzeug Das Fahrzeug, was dem Register hinzugefuegt werden soll.
+	 */
+	public void addFahrzeug(Fahrzeug fahrzeug) {
+		this.fahrzeuge.add(fahrzeug);
+	}
+
+	/**
+	 * Entfernt ein Fahrzeug, falls vorhanden, aus dem Register.
+	 * 
+	 * @param fahrzeug Das Fahrzeug, was aus dem Register geloescht werden soll.
+	 */
+	public void removeFahrzeug(Fahrzeug fahrzeug) {
+		this.fahrzeuge.remove(fahrzeug);
+	}
+
+	/**
+	 * Gibt die Liste aller registrierten Fahrzeuge zurueck.
+	 * 
+	 * @return Die Liste aller registrierten Fahrzeuge.
+	 */
+	public HashSet<Fahrzeug> getFahrzeuge() {
+		return this.fahrzeuge;
+	}
 }
